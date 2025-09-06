8afd2b6b022dc4983586cd3b2e10e601429c0e86
vmanstudios <vmancompany@gmail.com>
2025-09-06 19:52:19 +0100

    ECHOVIBE contest
    EchoVibe Singing Competition celebrates raw vocal talent and musical creativity. Soloists and groups light up the stage with diverse styles and powerful performances. With vibrant energy, audience cheers, and unforgettable moments, EchoVibe turns passion into a musical spectacle.

---
+++ script.js
@@ -0,0 +1,8 @@
+function showThanks() {
+  alert("https://chat.whatsapp.com/BusiKEqPBvf6hzveMewT9l?mode=ac_t.");
+  
+}
+document.getElementById("contactForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("Thanks for reaching out! We'll get back to you soon.");
+});
